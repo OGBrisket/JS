eaeda949949620a69a6b42ef3017769c3da6dccb
Matthew Pelnik <matthew.pelnik@icloud.com>
2022-10-24 19:39:31 -0500

    http server working!

--- Hello World/app.js
+++ Hello World/app.js
@@ -3,6 +3,7 @@ console.log(__dirname)
 // console.log(module)
 // console.log(process)
 
+
 // brings in the names from myCards
 const names = require('./myCards.js')
 console.log(names)
@@ -11,6 +12,13 @@ const hello = (name) => {
     console.log(`Hello there ${name}`);
 }
 
-// hello(firstCard)
 
-// Every file is a module
+// Bring in HTTP module
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.write('Welcome to my homepage!')
+    res.end()
+})
+
+server.listen(521)
