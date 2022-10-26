dc31cf2d7caecd66ef2320885841627e74747041
Matthew Pelnik <matthew.pelnik@icloud.com>
2022-10-25 19:01:10 -0500

    loaded NPM packages

---
+++ Hello World/usenpm.js
@@ -0,0 +1,11 @@
+// npm i package; for local
+// npm install -g package; for global
+
+// package.json - manifest file
+// create first with npm init
+
+const _ = require('lodash')
+
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
